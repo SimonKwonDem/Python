87961e76f41f142f46251fb79f995326a2461958
Simon Kwon <simonkwon03@gmail.com>
2025-12-22 19:53:15 -0300

    clase 01

---
+++ 00_hola.py
@@ -0,0 +1,8 @@
+#Comentario
+print('Hola Mundo')
+print(type("Hola Mundo"))
+"""
+Comentario en varias 
+lineas
+"""
+
---
+++ 01_variables.py
@@ -0,0 +1,16 @@
+#variables
+my_string_variable = "String"
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+print(type(my_int_variable))
+#concatenación de cadenas.
+print(my_string_variable, my_int_variable)
+
+#Tipo: none_type
+print(type(print(my_string_variable, my_int_variable)))
+
+#Algunas funciones del sistema
+#len() cuenta caracteres de un string
+print(len(my_string_variable))
