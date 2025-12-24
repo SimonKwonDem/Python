7414212b1d829e237f0bff9f8c2c163c00d65bae
Simon Kwon <simonkwon03@gmail.com>
2025-12-24 13:32:33 -0300

    dia 2

--- 01_variables.py
+++ 01_variables.py
@@ -14,3 +14,13 @@ print(type(print(my_string_variable, my_int_variable)))
 #Algunas funciones del sistema
 #len() cuenta caracteres de un string
 print(len(my_string_variable))
+
+#Variables en una sola línea
+name, surname, alias, age = "Simón", "Kwon", "Saimon", 22
+print("My name is:", name, surname, "My age is", age, "and people say", alias)
+
+#Inputs
+name = input("¿Cuál es tu nombre?")
+age = input("¿Cuántos años tienes?")
+print(name)
+print(age)
---
+++ 02_operadores.py
@@ -0,0 +1,19 @@
+#OPERADORES Aritméticos
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(2 / 3)
+print(3 % 2)
+print(10 // 3)
+print(2 ** 3)
+
+print("Hola " + "Python " + "¿como va? ")
+print("Simón " * 3)
+
+#Operadores Comparativos
+print(2 > 3)
+print(2 < 3)
+print(2 >= 3)
+print(2 <= 3)
+print(3 == 2)
+print(3 != 2)
